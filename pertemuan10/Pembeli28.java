8168b1fd42fe6e12584114f2972f2761784fcf13
leonanda1013 <leonandaprabowo@gamil.com>
2024-05-02 01:10:55 +0700

    update

---
+++ pertemuan10/Pembeli28.java
@@ -0,0 +1,15 @@
+package pertemuan10;
+
+public class Pembeli28{
+    String nama;
+    int noHP;
+
+    Pembeli28() {
+
+    }
+
+    Pembeli128 (String nama, int noHP) {
+        this.nama = nama;
+        this.noHP = noHP;
+    }
+}
