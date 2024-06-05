9e941549fe8e6f916d4dc20a9850b8313887edb2
leonanda1013 <leonandaprabowo@gamil.com>
2024-06-05 13:10:03 +0700

    upload

---
+++ pertemuan14/Percobaan1/DoubleLinkedList.java
@@ -0,0 +1,131 @@
+package pertemuan14.Percobaan1;
+
+public class DoubleLinkedList {
+
+    Node head;
+    int size;
+
+    public DoubleLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(int item, int jarak) {
+        if (isEmpty()) {
+            head = new Node(null, item, jarak, null);
+        } else {
+            Node newNode = new Node(null, item, jarak, head);
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(int item, int jarak) {
+        if (isEmpty()) {
+            addFirst(item, jarak);
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            Node newNode = new Node(current, item, jarak, null);
+            current.next = newNode;
+            size++;
+        }
+    }
+
+    public void add(int item, int jarak, int index) throws Exception {
+        if (isEmpty()) {
+            addFirst(item, jarak);
+        } else if (index < 0 || index > size) {
+            throw new Exception("Nilai indeks diluar batas");
+        } else {
+            Node current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.prev == null) {
+                Node newNode = new Node(null, item, jarak, current);
+                current.prev = newNode;
+                head = newNode;
+            } else {
+                Node newNode = new Node(current.prev, item, jarak, current);
+                newNode.prev = current.prev;
+                newNode.next = current;
+                current.prev.next = newNode;
+                current.prev = newNode;
+            }
+        }
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node tmp = head;
+            while (tmp != null) {
+                System.out.print(tmp.data + "\t");
+                tmp = tmp.next;
+            }
+            System.out.println("\nberhasil diisi");
+        } else {
+            System.out.println("Linked List Kosong");
+        }
+    }
+
+    public int getJarak(int index) throws Exception {
+        if (isEmpty() || index <= size) {
+            throw new Exception("Nilai indeks diluar batas");
+        }
+        Node tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.jarak;
+    }
+
+    public void remove(int index) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == index) {
+                if (current.prev != null) {
+                    current.prev.next = current.next;
+                } else {
+                    head = current.next;
+                }
+                if (current.next != null) {
+                    current.next.prev = current.prev;
+                }
+                break;
+            }
+            current = current.next;
+        }
+
+    }
+
+    public int get(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        }
+        Node tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+}
---
+++ pertemuan14/Percobaan1/GraphMain28.java
@@ -0,0 +1,15 @@
+package pertemuan14.Percobaan1;
+
+public class GraphMain28 {
+    public static void main(String[] args) {
+        Graph gedung = new Graph(6);
+        gedung.addEdge(0, 1, 50);
+        gedung.addEdge(0, 2, 100);
+        gedung.addEdge(1, 3, 70);
+        gedung.addEdge(2, 3, 40);
+        gedung.addEdge(3, 4, 60);
+        gedung.addEdge(4, 5, 80);
+        gedung.degree(0);
+        gedung.printGraph();
+    }
+}
